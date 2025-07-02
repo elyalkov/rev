287b52b28cea8247ef64bd3f5c12b2e68f5601f6
e.Lyalkov <e_lyalkov@mail.ru>
2025-07-02 10:54:00 +0300

    + get_absolute_url (убрал корзину из detail)

--- reverence/main/models.py
+++ reverence/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.urls import reverse
+#reverse — это функция для генерации URL по имени маршрута (удобно, т.к. не "зашиваем" URL вручную) - строит URL на основе имени маршрута из urls.py
 
 class Size(models.Model):
     name = models.CharField(max_length=10, unique=True)
@@ -14,21 +15,22 @@ class Category(models.Model):
     slug = models.SlugField(unique=True)
 
 
-    def __str__(self):
-        return self.name
-
-
     class Meta:
         ordering = ['name']
         indexes = [models.Index(fields=['name'])] #создаем индекс в базе данных по полю name для более быстрого поиска
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
+        def __str__(self):
+            return self.name
+
+    def get_absolute_url(self):
+        return reverse('main:product_list_by_category', args=[self.slug])
+#найти URL с именем product_list_by_category в пространстве имён main, и подставить туда self.slug
 
 class ClothingItem(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     available = models.BooleanField(default=True)
     sizes = models.ManyToManyField(Size, through='ClothingItemSize', #through - имя связующей таблицы
                                    related_name='clothing_item', blank=True) #related_name - как обращаться к элементу
@@ -43,6 +45,10 @@ class ClothingItem(models.Model):
 
     def __str__(self):
         return self.name
+
+
+    def get_absolute_url(self):
+        return reverse('main:product_detail', args=[self.id, self.slug])
 
 
     def get_price_with_discount(self):
